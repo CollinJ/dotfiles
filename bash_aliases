4640b5855c2b8adbdd5db7d67d001b4ffdb4682b
Collin Johnston <collin389@gmail.com>
2014-01-13 17:21:49 -0800

    added .bash and used rsync

--- bash_aliases
+++
@@ -1,3 +0,0 @@
-alias gcal=gcalcli
-alias lock='gnome-screensaver-command --lock'
-alias sus='gnome-screensaver-command --lock && sudo pm-suspend'
--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
-for FILE in bashrc vimrc bash_aliases Xresources
-do
-  rm -i ~/.$FILE
-  if [ ! -e ~/.$FILE ]; then
-    echo "Created symlink from ~/.$FILE to ~/dotfiles/$FILE"
-    ln -s ~/dotfiles/$FILE ~/.$FILE
-  fi
-done
-
+rm .*~
+rsync -av --exclude .git --exclude README.md --exclude install.sh . ~/
